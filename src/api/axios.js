ae3d4222da9939c44c129fff5ac1f07ae9710dec
musfeerakhalid375-source <musfeerakhalid375@gmail.com>
2026-07-28 17:46:05 +0500

    Connect frontend to live backend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://crudbackend-gky2.onrender.com//api"
+    baseURL: "https://crudbackend-gky2.onrender.com/api"
 })
 
 export default api
