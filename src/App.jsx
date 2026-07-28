8f35644cb34602184c12a35eb79077cce7109c63
musfeerakhalid375-source <musfeerakhalid375@gmail.com>
2026-07-28 18:45:45 +0500

    / register

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,13 @@ import Navbar from './component/Navbar'
 import { Route, Routes , Navigate  } from 'react-router-dom'
 import Login from './component/Login'
 import ContactApp from './component/ContactApp'
-import Home from './component/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
 <Routes>
-   <Route path='/' element={<Navigate to={'/users'}/>} /> 
-   
+   <Route path='/' element={<Navigate  to="/register" />} /> 
   <Route path='/register' element={<Register/>} />
   <Route path='/register/:id' element={<Register/>} /> 
   <Route path='/login' element={<Login/>} />
