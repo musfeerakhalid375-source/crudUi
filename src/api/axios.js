8ce65d1bd485cb027c9644d03c6866d959519fb6
musfeerakhalid375-source <musfeerakhalid375@gmail.com>
2026-07-29 18:22:48 +0500

    update granetae token connect

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://crudbackend-gky2.onrender.com/api"   
-    // baseURL: " http://localhost:5000/api"
+    baseURL: "https://crudbackend-gky2.onrender.com/api"  , 
+    // baseURL: "http://localhost:5000/api", 
+    withCredentials: true   //  is ke bina cookie kabhi bhejta hi nahi
 })
 
 export default api
